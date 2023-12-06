f054512baecc597547ad67ab5b0c8d09bb09c0d1
bblackrabbit <gilaziev7556@gmail.com>
2023-12-06 20:09:35 +0400

    Калькулятор комплексных чисел

--- js/calculators/ComplexCalculator.js
+++ js/calculators/ComplexCalculator.js
@@ -1,5 +1,21 @@
-class Complex{
-    constructor (re = 0, im = 0)
-    this.re = re
-    
+class ComplexCalculator extends RealCalculator {
+    add(a, b) { return new Complex( a.re + b.re, a.im + b.im ); }
+    sub(a, b) { return new Complex( a.re - b.re, a.im - b.im ); }
+    mult(a, b) { return new Complex( a.re*b.re - a.im*b.im, a.re*b.im + b.re*a.im ); }
+    inv(a) {
+        const q = Math.sqrt(a.re) + Math.sqrt(a.im);
+        return new Complex( a.re / q, -a.im / q );
+    }
+    div(a, b) {
+        return this.mult( a, this.inv(b) );}
+    pow(a, n) {
+        let S = this.one();
+        for (let i=0; i < n; i++) {
+            S = this.mult(S, a);
+        }
+        return S;
+    }
+    prod(a, p) { return new Complex( a.re * p, a.im * p); }
+    zero() { return new Complex(); }
+    one() { return new Complex( super.one() ); }
 }
--- js/calculators/RealCalculator.js
+++ js/calculators/RealCalculator.js
@@ -1,14 +1,18 @@
 class RealCalculator {
-    add(a,b){return a+b;}
-    sub(a,b){return a-b;}
-    mult(a,b){return a*b;}
-    div(a,b){return a/b;}
-    pow(a,n){return Math.pow(a,n);}
-    prod(a,p){return a*p;}
-    one(){
+    add(a, b) { return a + b; }
+    sub(a, b) { return a - b; }
+    mult(a, b) { return a * b; }
+    div(a, b) {
+        if (b != 0)
+            return a / b;
+        else return 'Ошибка!';
+    }
+    pow(a, n) { return Math.pow(a, n); }
+    prod(a, p) { return a * p; }
+    one() {
         return 1;
     }
-    zero(){
+    zero() {
         return 0;
     }
 }
